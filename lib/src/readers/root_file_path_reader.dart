f539da21073600f9dce1867fda50789dea1da12c
David Hobley <david.hobley@sharpblue.com.au>
2025-07-21 14:51:23 +1000

    use startsWith instead of equality for better epub support

--- lib/src/readers/root_file_path_reader.dart
+++ lib/src/readers/root_file_path_reader.dart
@@ -9,18 +9,15 @@ class RootFilePathReader {
   static Future<String?> getRootFilePath(Archive epubArchive) async {
     const EPUB_CONTAINER_FILE_PATH = 'META-INF/container.xml';
 
-    var containerFileEntry = epubArchive.files.firstWhereOrNull(
-        (ArchiveFile file) => file.name == EPUB_CONTAINER_FILE_PATH);
+    var containerFileEntry = epubArchive.files.firstWhereOrNull((ArchiveFile file) => file.name.contains(EPUB_CONTAINER_FILE_PATH));
     if (containerFileEntry == null) {
       throw Exception(
           'EPUB parsing error: $EPUB_CONTAINER_FILE_PATH file not found in archive.');
     }
 
-    var containerDocument =
-        xml.XmlDocument.parse(convert.utf8.decode(containerFileEntry.content));
+    var containerDocument = xml.XmlDocument.parse(convert.utf8.decode(containerFileEntry.content));
     var packageElement = containerDocument
-        .findAllElements('container',
-            namespace: 'urn:oasis:names:tc:opendocument:xmlns:container')
+        .findAllElements('container', namespace: 'urn:oasis:names:tc:opendocument:xmlns:container')
         .firstWhereOrNull((xml.XmlElement? elem) => elem != null);
     if (packageElement == null) {
       throw Exception('EPUB parsing error: Invalid epub container');
